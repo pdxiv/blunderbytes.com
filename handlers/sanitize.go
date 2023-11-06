42c127d310518ff2bcaf5a4b0b05f1c6fca47757
pdxiv <pdxiv@github.com>
2023-11-06 09:13:17 +0100

    allow markdown characters

--- handlers/sanitize.go
+++ handlers/sanitize.go
@@ -9,7 +9,8 @@ const MAX_INPUT_LENGTH = 255
 // sanitizeInput takes a string and ensures it only contains allowed characters.
 // It also trims the input to a maximum length of 255 characters.
 func sanitizeInput(input string) string {
-	allowedPattern := regexp.MustCompile(`[^\w\n:,.!-/=\?"-]+`)
+	// Extended the pattern to include Markdown characters such as *, _, ~, `, [], and ()
+	allowedPattern := regexp.MustCompile(`[^\w\n:,.!/=\?"#*+~` + "`" + `\[\]()-]+`)
 	if len(input) > MAX_INPUT_LENGTH {
 		input = input[:MAX_INPUT_LENGTH]
 	}
